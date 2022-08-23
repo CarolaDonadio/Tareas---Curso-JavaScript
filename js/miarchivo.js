5d447227c3d0a0c4eae22aac22d815381ab38792
Carola Donadío <caroladonadio9@gmail.com>
2022-08-23 12:27:06 -0300

    ejercicio 1 de JS

---
+++ js/miarchivo.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// Algoritmo que pida al usuario ingresar un número entero y después que se muestre por document.write 15 veces el número elegido.
+
+let numEntero = parseInt(prompt("Ingrese un número entero:", 0));
+
+while (isNaN(numEntero)) {
+    numEntero = parseInt(prompt("Ingrese un número entero:", 0));
+}
+
+for (let i = 0; i < 15; i++) {
+    document.write(numEntero);
+}
+
+//Algoritmo que escriba la tabla de multiplicar de un número ingresado por el usuario:
+
+let numero = parseInt(prompt("Table del "));
+let prefijo = numero + "x";
+let producto;
+let mensaje;
+
+for (let contador = 0; contador <= 10; contador++) {
+    producto = numero * contador;
+    mensaje = prefijo + contador + " = " + producto + "," + "\n";
+    document.write(mensaje);
+}
